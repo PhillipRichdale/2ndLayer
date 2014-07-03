3e78350ab889274b1c076dda11bd07aff7f93d10
actcontpr <actcontpr@googlemail.com>
2014-07-03 14:36:04 +0200

    cleanup of header

--- classes/Generator.php
+++ classes/Generator.php
@@ -1,15 +1,9 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  * Description of Generator
  *
- * @author phillip
+ * @author actcontpr
  */
 class Generator {
 	private $db;
