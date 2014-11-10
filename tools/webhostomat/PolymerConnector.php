37a532a870368a19c47ff8fbbac6968ca11096bd
actcontpr <actcontpr@googlemail.com>
2014-11-10 15:55:20 +0100

    work in progress (wip)

---
+++ tools/webhostomat/PolymerConnector.php
@@ -0,0 +1,13 @@
+<?php
+
+# This class handles the communiction of the backend to the frontend in the webhostomat tool.
+# The UI (frontend) is built in Polymer, hence the name.
+
+class PolymerConnector
+{
+	public function __construct(){}
+	public function getMessage()
+	{
+		
+	}
+}
---
+++ tools/webhostomat/SiteConfigHandler.php
@@ -0,0 +1,45 @@
+<?php
+
+class SiteConfigHandler
+{
+	private $templatePath = "/etc/apache2/sites-available/template";
+	
+	public $validDirsFile = false;
+	public $domainString = false;
+	public $newConfigFileName = false;
+	
+	public function __construct(
+		$validDirsFile,
+		$domainString,
+		$newConfigFileName
+	){
+		$this->validDirsFile = $validDirsFile;
+		$this->domainString = $domainString;
+		$this->newConfigFileName = $newConfigFileName;
+	}
+	public function processAndSaveNewConfig()
+	{
+		$newConfig = file_get_contents($this->templatePath);
+		$newConfig = str_replace("###DOMAIN_STRING###", $this->domainString, $newConfig);
+		$newConfig = str_replace("###DOCUMENT_ROOT###", $this->docRoot, $newConfig);
+		file_put_contents("/etc/apache2/sites-available/".$this->newConfigFileName , $newConfig);
+	}
+	public function listValidDirectories()
+	{
+		$callback = $this->checkDirPath($path);
+		$temp = file($this->validDirsFile);
+		$temp = array_filter($temp, $callback);
+		return $temp;
+	}
+	public function restartApache()
+	{
+		$pause = 10;
+		$message = exec("service apache2 restart");
+		sleep($pause);
+		return "Apache service restartet $pause seconds ago. Message: '".$message."'.";
+	}
+	private function checkDirPath($path)
+	{
+		return strlen($path) > 2;
+	}
+}
