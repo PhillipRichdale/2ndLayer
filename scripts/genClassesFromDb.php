a5f34d7b155c2edea676609604022bd181d624ba
actcontpr <actcontpr@googlemail.com>
2015-04-01 17:06:57 +0200

    bugfixing (broken script adapted to new seperate Generator, now also passed PDO connection correctly)

--- scripts/genClassesFromDb.php
+++ scripts/genClassesFromDb.php
@@ -4,6 +4,14 @@
 	// how the DbConnector and Generator classes are used. You probably
 	// won't need any other script though.
 
-	require '../classes/DbConnector.php';
-	$db = new DbConnector();
-	$db->genClassesFromDb();
+	require 'classes/DbConnector.php';
+	require 'classes/Generator.php';
+	$db = new DbConnector(
+					$dbServerType="mysql",
+					$dbHost="127.0.0.1",
+					$dbPort="3306",
+					$dbName="",
+					$dbUser="",
+					$dbPw="");
+	$gen = new Generator($db->getPdoConnection());
+	$gen->genClassesFromDb();
